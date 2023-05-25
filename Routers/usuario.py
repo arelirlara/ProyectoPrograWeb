e2386b87238d397f7d1681a949bdd0b913104ddc
arelirlara <105070510+arelirlara@users.noreply.github.com>
2023-05-24 19:49:00 -0600

    api get productos y sucursales para usuarios

---
+++ Routers/usuario.py
@@ -0,0 +1,16 @@
+from fastapi import APIRouter, Request
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+
+router = APIRouter(prefix="/tulip", tags=["tulip"], responses={404: {"message": "Sin usuarios registrados."}})
+router.mount("/static", StaticFiles(directory="static"), name="static")
+plantillas = Jinja2Templates(directory="plantillas")
+
+@router.get("/catalogo", response_class=HTMLResponse)
+async def catalogo(request: Request):
+    return plantillas.TemplateResponse("catalogo_usuario.html", {"request": request})
+
+@router.get("/contacto", response_class=HTMLResponse)
+async def sucursales(request: Request):
+    return plantillas.TemplateResponse("contacto_usuario.html", {"request": request})
--- api.py
+++ api.py
@@ -1,12 +1,30 @@
 import uvicorn
 from fastapi import FastAPI
-from Routers import autenticacion, catalogo, sucursales
+from Routers import autenticacion, catalogo, usuario
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
 app.include_router(autenticacion.router)
 app.include_router(catalogo.router)
-app.include_router(sucursales.router)
+app.include_router(usuario.router)
+
+# Configurar los orígenes permitidos en CORS
+origins = [
+    "http://localhost",
+    "http://localhost:8000",
+    "http://localhost:3000",
+    # Agrega aquí otros orígenes permitidos
+]
+
+# Habilitar el middleware de CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 if __name__ == "__main__":
-    uvicorn.run(app)
+    uvicorn.run(app, host="0.0.0.0", port=8000)
